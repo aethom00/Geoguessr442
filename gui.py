47f80ddca65da473389bcbefdd9cfd4ddf1d41f1
Davidch4r <dpidu001@gmail.com>
2023-11-02 20:51:16 -0400

    Started jupyter

--- gui.py
+++ gui.py
@@ -3,58 +3,82 @@ import matplotlib.patches as patches
 from PIL import Image
 import random
 import numpy as np
+from scipy.special import softmax
+from scipy.signal import normalize
+
+class GUI:
+    def __init__(self, num_rects_width, num_rects_height):
+        self.image_loc = 'america.png'
+        im = Image.open(self.image_loc)
+        self.width, self.height = im.size
+        im.close()
+
+        self.num_rects_width = num_rects_width
+        self.num_rects_height = num_rects_height
+
+        self.square_amount = (self.width / num_rects_width, self.height / num_rects_height)
+
+        self.extreme_points = {'N': 50, 'S': 24, 'E': -66, 'W': -126}
+        self.total_lat = self.extreme_points['N'] - self.extreme_points['S']
+        self.total_long = self.extreme_points['W'] - self.extreme_points['E']
+
+        self.output = np.array([[0 for _ in range(self.num_rects_width)] for _ in range(self.num_rects_height)])
+
+        self.locations = {}
+
+    def pixel_loc_to_lat_long(self, w, h):
+        return (round(self.extreme_points['N'] - h/self.height * (self.total_lat), 2), round(self.extreme_points['W'] - w/self.width * (self.total_long), 2))
+
+    def long_lat_to_index(self, long, lat):
+        # TODO
+        # Convert Coordinates to the cloest 2d index that can be used in the locations dictionary
+        return ()
+    
+    def init(self):
+        self.x_ticks = [i * self.square_amount[0] for i in range(self.num_rects_width + 1)]
+        self.y_ticks = [j * self.square_amount[1] for j in range(self.num_rects_height + 1)]
+        self.x_labels = [self.pixel_loc_to_lat_long(tick, 0)[1] for tick in self.x_ticks]
+        self.y_labels = [self.pixel_loc_to_lat_long(0, tick)[0] for tick in self.y_ticks]
+
+        for i, w in enumerate(range(self.num_rects_width)):
+            for j, h in enumerate(range(self.num_rects_height)):
+                x, y = (w * self.square_amount[0], h * self.square_amount[1])
+                self.locations[(i, j)] = self.pixel_loc_to_lat_long(x + 0.5 * self.square_amount[0], y + 0.5 * self.square_amount[1])
+                
+    
+    def show(self, display_coords=False):     
+        fig, ax = plt.subplots()
+        ax.imshow(Image.open(self.image_loc))
+
+        ax.set_xticks(self.x_ticks)
+        ax.set_yticks(self.y_ticks)
+
+        ax.set_xticklabels(self.x_labels, rotation=90)
+        ax.set_yticklabels(self.y_labels)
+
+        ax.set_xlabel("Longitude")
+        ax.set_ylabel("Latitude")
+
+        for i, w in enumerate(range(self.num_rects_width)):
+            for j, h in enumerate(range(self.num_rects_height)):
+                x, y = (w * self.square_amount[0], h * self.square_amount[1])
+                ax.add_patch(patches.Rectangle(
+                                            (x, y), 
+                                            self.square_amount[0], self.square_amount[1], 
+                                            fc=(1, 0, 0, self.output[j, i] * 0.5),  
+                                            ec='none', 
+                                            lw=1))
+                if display_coords:
+                    plt.text(x, y + 0.5 * self.square_amount[1], self.locations[(i, j)], fontsize=5 * 10/self.num_rects_width)                        
+
+        plt.tight_layout()  
+        plt.show()
 
 
-im = Image.open('america.png')
-width, height = im.size
+    def generate_random_output(self):
+        self.output = np.array([[random.uniform(0, 1) for _ in range(self.num_rects_width)] for _ in range(self.num_rects_height)])
 
-num_rects_width = 13
-num_rects_height = 10
-square_amount = (width / num_rects_width, height / num_rects_height)
+    def clear_output(self):
+        self.output.fill(0)
 
-extreme_points = {'N': 50, 'S': 24, 'E': -66, 'W': -126}
-total_lat = extreme_points['N'] - extreme_points['S']
-total_long = extreme_points['W'] - extreme_points['E']
 
-def pixel_loc_to_lat_long(w, h):
-    return (round(extreme_points['N'] - h/height * (total_lat), 2), round(extreme_points['W'] - w/width * (total_long), 2))
-
-def long_lat_to_index(long, lat):
-    # TODO
-    # Convert Coordinates to the cloest 2d index that can be used in the locations dictionary
-    return ()
-
-locations = {}
-
-x_ticks = [i * square_amount[0] for i in range(num_rects_width + 1)]
-y_ticks = [j * square_amount[1] for j in range(num_rects_height + 1)]
-x_labels = [pixel_loc_to_lat_long(tick, 0)[1] for tick in x_ticks]
-y_labels = [pixel_loc_to_lat_long(0, tick)[0] for tick in y_ticks]
-
-fig, ax = plt.subplots()
-ax.imshow(im)
-
-ax.set_xticks(x_ticks)
-ax.set_yticks(y_ticks)
-
-ax.set_xticklabels(x_labels, rotation=90)
-ax.set_yticklabels(y_labels)
-
-output = np.array([[random.uniform(0, 1) for _ in range(num_rects_width)] for _ in range(num_rects_height)])
-
-for i, w in enumerate(range(num_rects_width)):
-    for j, h in enumerate(range(num_rects_height)):
-        x, y = (w * square_amount[0], h * square_amount[1])
-        ax.add_patch(patches.Rectangle((x, y), 
-                                      square_amount[0], square_amount[1], 
-                                      fc=(1, 0, 0, output[j, i] * 0.5),  
-                                      ec='none', 
-                                      lw=1))
-        locations[(i, j)] = pixel_loc_to_lat_long(x + 0.5 * square_amount[0], y + 0.5 * square_amount[1])
-        plt.text(x, y + 0.5 * square_amount[1], locations[(i, j)], fontsize=5 * 10/num_rects_width)
-
-ax.set_xlabel("Longitude")
-ax.set_ylabel("Latitude")
-
-plt.tight_layout()  
-plt.show()
