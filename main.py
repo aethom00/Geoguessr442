9442a4dd697d14986c1c60c5a0c5941e97ed8f9c
Davidch4r <dpidu001@gmail.com>
2023-12-19 14:08:20 -0500

    Restarted Model Completely. RIP VM1[OG]

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from Agent import Agent
 import time
 import os
 
-def log_data(total_elapsed_time, total_epochs, epoch_per_epochs, images_per_epoch, loss, accuracy, folder='Logs'):
+def log_data(model_name, total_elapsed_time, total_epochs, epoch_per_epochs, images_per_epoch, loss, accuracy, folder='Logs'):
     # Log filename format: [date]_[time]_log.txt
     log_filename = f"{time.strftime('%Y-%m-%d_%H-%M')}_log.txt"
-    log_path = os.path.join(folder, log_filename)
+    log_path = os.path.join(folder, model_name, log_filename)
 
-    # Ensure the 'Logs' folder exists
     if not os.path.exists(folder):
         os.makedirs(folder)
 
+    if not os.path.exists(os.path.join(folder, model_name)):
+        os.makedirs(os.path.join(folder, model_name))
+
     with open(log_path, 'w') as file:
+        file.write(f"Model name: {model_name}\n")
         file.write(f"Total elapsed time: {total_elapsed_time:.2f} seconds\n")
         file.write(f"Total epochs: {total_epochs}\n")
         file.write(f"Epochs per epoch: {epoch_per_epochs}\n")
@@ -19,8 +22,9 @@ def log_data(total_elapsed_time, total_epochs, epoch_per_epochs, images_per_epoc
         file.write(f"Loss: {loss}\n")
         file.write(f"Accuracy: {accuracy}\n")
 
+model_name = 'vision_model_v1'
 agent = Agent()
-agent.init(model_path='vision_model_v1.keras')
+agent.init(model_path=f"{model_name}.keras")
 
 total_elapsed_time, total_epochs, epoch_per_epochs, images_per_epoch = (
 agent.train_model(
@@ -36,5 +40,5 @@ loss, accuracy = (
 agent.evaluate_model(images_to_test=100, use_noisy_images=False, show=False)
 )
 
-log_data(total_elapsed_time, total_epochs, epoch_per_epochs, images_per_epoch, loss, accuracy)
+log_data(model_name, total_elapsed_time, total_epochs, epoch_per_epochs, images_per_epoch, loss, accuracy)
 
