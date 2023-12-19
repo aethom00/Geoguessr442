375f8196e6cfe2dfa6245deb42940f62513308cf
Davidch4r <dpidu001@gmail.com>
2023-12-19 18:38:37 -0500

    Trained on more images

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ agent.init(model_path=f"{model_name}.keras")
 
 total_elapsed_time, total_epochs, epoch_per_epochs, images_per_epoch = (
 agent.train_model(
-    total_epochs=100,                  # Total number of epochs to train for
+    total_epochs=250,                  # Total number of epochs to train for
     epoch_per_epochs=3 ,               # Number of sub-epochs to train for [Utiziling the same images of that epoch]
     batch_size=32,                     # Batch size for training
-    images_per_epoch=10,               # How many images to generate per epoch
+    images_per_epoch=20,               # How many images to generate per epoch
     use_noisy_images=False,            # Whether to use noisy images or not
     save_after_epochs=10,              # Save the model after every x epochs
 ))
